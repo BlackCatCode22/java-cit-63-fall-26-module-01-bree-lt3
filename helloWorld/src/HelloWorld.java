625ec4831bbbec26f303fce30b4fc3e1002241ea
bree-lt3 <52141078+bree-lt3@users.noreply.github.com>
2026-08-18 16:07:31 -0700

    actual helloWorld formatting fix

--- helloWorld/src/HelloWorld.java
+++ helloWorld/src/HelloWorld.java
@@ -1,5 +1,13 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
+/*
+  Breanna Rayburn
+  File: HelloWorld.java
+  Date: 08/18/2026
+*/
+
+public class HelloWorld 
+{
+  public static void main(String[] args) 
+  {
+    System.out.println("Hello World!");
   }
 }
