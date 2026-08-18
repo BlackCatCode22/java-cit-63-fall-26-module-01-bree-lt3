c34f1d747dcae69bdfe4f769be98cac1cdad150a
bree-lt3 <52141078+bree-lt3@users.noreply.github.com>
2026-08-18 15:56:56 -0700

    fix helloWorld formatting and add interest app

---
+++ interest/src/Interest.java
@@ -0,0 +1,26 @@
+/*
+  Breanna Rayburn
+  File: SumOfTwoInts.java
+  Date: 08/18/2026
+*/
+
+public class Interest 
+{
+  public static void main(String[] args)
+  {
+    double principal;
+    double rate;
+    double interest;
+
+    principal = 17000;
+    rate = 0.027;
+    interest = principal * rate;
+
+    principal = principal + interest;
+
+    System.out.print("The interest earned is $");
+    System.out.println(interest);
+    System.out.print("The value of the investment after one year is $");
+    System.out.println(principal);
+  }
+}
